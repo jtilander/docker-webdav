cd76e82881a0a2e29b85c0ca1c0a0c1a3658761c
Jim Tilander <jim@tilander.org>
2018-01-24 20:51:00 -0800

    Enabling json listing by default

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,10 +7,14 @@ if [ "$1" = "nginx" ]; then
 	export WORKER_COUNT=${WORKER_COUNT:-4}
 	export WORKER_CONNECTIONS=${WORKER_CONNECTIONS:-1024}
 	export WORKER_USERNAME=${WORKER_USERNAME:-www-data}
-	
+	export LISTFORMAT=${LISTFORMAT:-json}
+
+	export SENDFILE=${SENDFILE:-on}
+	export TCP_NOPUSH=${TCP_NOPUSH:-off}
+
 	chown -R ${WORKER_USERNAME} /data
 
-	envsubst '${WORKER_COUNT} ${WORKER_CONNECTIONS} ${WORKER_USERNAME}' > /etc/nginx/nginx.conf < /etc/nginx/nginx.conf.tmpl
+	envsubst '${WORKER_COUNT} ${WORKER_CONNECTIONS} ${WORKER_USERNAME} ${LISTFORMAT} ${SENDFILE} ${TCP_NOPUSH}' > /etc/nginx/nginx.conf < /etc/nginx/nginx.conf.tmpl
 
 	exec /usr/sbin/nginx -g "daemon off;"
 
